ada661497ff642fb153fe5c8aff118d7513f58d4
Maura Kummer <maurkummer@yahoo.com>
2022-05-02 13:12:32 +0200

    correcting syntax for pushing an object to an array

---
+++ PushObjToArray/app3.js
@@ -0,0 +1,8 @@
+/*This always returns an error,:
+items = []
+items.push {a: "b", c: "d"}
+
+what is the correct way of pushing the object to th empty array*/
+
+items = [];
+items.push({ a: 'b', c: 'd' });
