ce1c45a5850b2a1a85a784e8f6d205d9beb2ba04
Maura Kummer <maurkummer@yahoo.com>
2022-05-04 06:26:36 +0200

    first commit for casting arrays into a CSV representation

--- VowelsCount/app11.js
+++ VowelsCount/app11.js
@@ -6,18 +6,23 @@ The input string will only consist of lower case letters and/or spaces.
 
 */
 
-
-function getCount(str){
+function getCount(str) {
     let vowelsCount = 0;
-    const vowels = ['a','e','i','o','u']
-    const strArr = str.split('')
-    
-    strArr.forEach((el)=>{
-      for(let i = 0; i < vowels.length; i++){
-        if(el=== vowels[i]){
-          vowelsCount++;
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    const strArr = str.split('');
+
+    strArr.forEach((el) => {
+        for (let i = 0; i < vowels.length; i++) {
+            if (el === vowels[i]) {
+                vowelsCount++;
+            }
         }
-      } 
     });
-    return vowelsCount
-    
+    return vowelsCount;
+}
+
+//Using the arrow function and filter
+
+function getCount(str) {
+    return str.split('').filter((el) => 'aeiouAEIOU'.includes(el)).length;
+}
